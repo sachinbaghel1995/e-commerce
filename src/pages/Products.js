1d2e9c57ba791dd4381d3ce0678aeb63186f062e
Sachin Baghel <baghel.sachin1996@gmail.com>
2023-03-29 15:53:27 +0530

    https://github.com/sachinbaghel1995/ecommerce-bootstrap

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,27 +1,27 @@
-import { useState } from "react";
-import HeaderCartButton from "../components/Cart/HeaderCartButton";
-import { NavLink } from "react-router-dom";
-import Cart from "../components/Cart/Cart";
-import ProductsDetail from "../components/Form/ProductsDetail";
-import CartProvider from "../components/Context/CartProvider";
+// import { useState } from "react";
+// import HeaderCartButton from "../components/Cart/HeaderCartButton";
+// import { NavLink } from "react-router-dom";
+// import Cart from "../components/Cart/Cart";
+// import ProductsDetail from "../components/Form/ProductsDetail";
+// import CartProvider from "../components/Context/CartProvider";
 
-const Products = () => {
-  const [showCart, setShowCart] = useState(false);
+// const Products = () => {
+//   const [showCart, setShowCart] = useState(false);
 
-  const showCartHandler = () => {
-    setShowCart(true);
-  };
-  const hideCartHandler = () => {
-    setShowCart(false);
-  };
-  return (
-    <div>
-      <CartProvider>
-        <HeaderCartButton onClick={showCartHandler} />
-        {showCart && <Cart onClose={hideCartHandler} />}
-        <ProductsDetail />
-      </CartProvider>
-    </div>
-  );
-};
-export default Products;
+//   const showCartHandler = () => {
+//     setShowCart(true);
+//   };
+//   const hideCartHandler = () => {
+//     setShowCart(false);
+//   };
+//   return (
+//     <div>
+//       <CartProvider>
+//         <HeaderCartButton onClick={showCartHandler} />
+//         {showCart && <Cart onClose={hideCartHandler} />}
+//         <ProductsDetail />
+//       </CartProvider>
+//     </div>
+//   );
+// };
+// export default Products;
